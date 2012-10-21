6cf1f88fdb655b12a4bbd9f1bfbd09a50cdb547a
Tobias Haar <tobias.haar@googlemail.com>
2012-10-21 10:00:45 +0100

    Revert "do not initialise application for asset precompilation"
    This reverts commit 9db38e0d0bf0597fec70b2de13fe40a9524d460b.

--- config/application.rb
+++ config/application.rb
@@ -59,8 +59,6 @@ module TyneHost
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.initialize_on_precompile = false
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
