fc4b9281651a0d046bf888814f2817efd2ada035
Tobias Haar <tobias.haar@googlemail.com>
2013-02-04 21:21:05 +0000

    creates a proper page title for most of the pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,21 @@ module ApplicationHelper
   def project_name
     content_tag :span, "#{@project.user.username}/#{@project.key}", :class => "brand-project" if @project
   end
+
+  def page_title
+    title = if content_for?(:title)
+      yield(:title)
+    else
+      options = {}
+      options[:key] = @project.key if @project
+      options[:user] = current_user.username if current_user
+      options[:user] = @user.username if @user
+      options[:user] = @project.user.username if @project
+      options[:number] = @issue.number if @issue
+
+      I18n.t("titles.#{controller.controller_name}.#{params[:action]}", options)
+    end
+
+    "#{title} - Tyne"
+  end
 end
