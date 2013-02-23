fae17c1965a35d41430f385bfd84ee1cbf02195c
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2013-02-23 18:25:51 +0000

    Extracted cache key generator

--- lib/extensions/active_record/relation.rb
+++ lib/extensions/active_record/relation.rb
@@ -7,14 +7,7 @@ module Extensions
 
       # Generate a cache key for active record relations
       def cache_key
-        if self.empty?
-          'empty/' + self.object_id.to_s
-        else
-          ids_hash = Digest::MD5.hexdigest(self.collect{|item| item.id }.to_s)
-          update_timestamp = max {|a,b| a.updated_at <=> b.updated_at }.updated_at.to_i.to_s
-          create_timestamp = max {|a,b| a.created_at <=> b.created_at }.created_at.to_i.to_s
-          self.first.class.to_s.tableize+'/'+length.to_s+'-'+ids_hash+'-'+create_timestamp+'-'+update_timestamp
-        end
+        CacheKeyGenerator.new.for_collection(self)
       end
     end
   end
