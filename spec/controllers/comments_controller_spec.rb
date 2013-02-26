4887a4b90cd69c161d1f0bf01ba7489c91cb591f
Tobias Haar <tobias.haar@googlemail.com>
2013-02-26 21:17:26 +0000

    fixes comments controller spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,19 +1,13 @@
 require 'spec_helper'
 
 describe CommentsController do
+  fixtures :projects, :users, :issues
+
+  let(:user) { users(:tobscher) }
+  let(:project) { projects(:tyne) }
+  let(:issue) { issues(:foo) }
+
   its (:is_admin_area?) { should be_false }
-
-  let(:user) do
-    User.create!(:name => "Foo", :username => "Foo", :uid => "foo", :token => "foo")
-  end
-
-  let(:project) do
-    user.projects.create!(:key => "Foo", :name => "Foo")
-  end
-
-  let(:issue) do
-    Issue.create!(:summary => "Foo", :project_id => project.id, :issue_type_id => 1)
-  end
 
   context :logged_in do
     before :each do
@@ -22,7 +16,7 @@ describe CommentsController do
 
     describe :create do
       before :each do
-        post :create, :user => user.username, :key => project.key, :issue_id => issue.id, :comment => { :message => "Foo" }, :format => :pjax
+        post :create, :user => user.username, :key => project.key, :issue_id => issue.number, :comment => { :message => "Foo" }, :format => :pjax
       end
 
       it "should render the comment view" do
