495fcc97e2340f9e77617a096caf1890c4ede750
Tobias Haar <tobias.haar@googlemail.com>
2013-02-26 23:08:39 +0000

    get rid of rspec/autorun to fix problems when using spring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,6 @@ end
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
 
