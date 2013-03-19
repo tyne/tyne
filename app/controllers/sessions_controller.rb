361690e9c42a6a08b55c804899e2b2e9b837a27b
Tobias Haar <tobias.haar@googlemail.com>
2013-03-19 18:46:46 +0000

    redirect the user to where the user wanted to go after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,24 +6,30 @@ class SessionsController < ApplicationController
   # It redirects to the root page
   def create
     auth_hash = request.env['omniauth.auth']
+    params_hash = request.env['omniauth.params']
 
     user = AuthProcessor.new(auth_hash).find_or_create_user
     session[:user_id] = user.id
 
-    redirect_to main_app.root_path, :notice => I18n.t("authentication.logged_in", :username => user.name)
+    redirect_to redirect_url_for_login, :notice => I18n.t("authentication.logged_in", :username => user.name)
   end
 
   # Logout the user
   # Redirects to the login page
   def destroy
     session[:user_id] = nil
-    redirect_to main_app.login_path, :notice => I18n.t("authentication.logged_out")
+    redirect_to login_path, :notice => I18n.t("authentication.logged_out")
   end
 
   # Displays an error message and redirect to the login page
   def failure
     flash[:error] = I18n.t("authentication.not_allowed")
 
-    redirect_to main_app.login_path
+    redirect_to login_path
+  end
+
+  private
+  def redirect_url_for_login
+    params_hash["redirect_url"] || root_path
   end
 end
