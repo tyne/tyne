aa8221015fabd5f1611ec367912c1d83d83a85e1
Tobias Haar <tobias.haar@googlemail.com>
2013-03-20 00:43:34 +0000

    do not raise errors for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Tyne::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
