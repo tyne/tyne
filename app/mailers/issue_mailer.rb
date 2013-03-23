596987be82df7d4af7f2d7d0bdbdcd92f2c9cb05
Tobias Haar <tobias.haar@googlemail.com>
2013-03-23 08:46:52 +0000

    load issue properly in issue_mailer

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -5,6 +5,7 @@ class IssueMailer < ActionMailer::Base
   # Sends a notification to all contributors
   # that a new issue has been raised.
   def self.send_issue_raised(issue_id)
+    issue = Issue.find_by_id(issue_id)
     issue.project.workers.each do |worker|
       delay.issue_raised(issue_id, worker.id)
     end
@@ -16,29 +17,29 @@ class IssueMailer < ActionMailer::Base
     @worker = TeamMember.find_by_id(worker.id)
     to = worker.user.notification_email
 
-    mail(:to => to, :subject => "[Raised] #{issue.key} - #{issue.summary}") if to
+    mail(:to => to, :subject => "[Raised] #{@issue.key} - #{@issue.summary}") if to
   end
 
   # Sends a notification to the reporter of the issue
   # that the issue has been closed.
-  def issue_closed(issue)
-    @issue = issue
+  def issue_closed(issue_id)
+    @issue = Issue.find_by_id(issue_id)
 
     # Send to reporter
-    if issue.reported_by
-      to = issue.reported_by.notification_email
-      mail(:to => to, :subject => "[Closed] #{issue.key} - #{issue.summary}") if to
+    if @issue.reported_by
+      to = @issue.reported_by.notification_email
+      mail(:to => to, :subject => "[Closed] #{@issue.key} - #{@issue.summary}") if to
     end
   end
 
   # Sends a notification to the assignee
   # that the issue has been reopened.
-  def issue_reopened(issue)
-    @issue = issue
+  def issue_reopened(issue_id)
+    @issue = Issue.find_by_id(issue_id)
     # Send to to assignee
-    if issue.assigned_to
-      to = issue.assigned_to.notification_email
-      mail(:to => to, :subject => "[Reopened] #{issue.key} - #{issue.summary}") if to
+    if @issue.assigned_to
+      to = @issue.assigned_to.notification_email
+      mail(:to => to, :subject => "[Reopened] #{@issue.key} - #{@issue.summary}") if to
     end
   end
 end
