9a9d43c84b16f3e3abff7f01cddebfe65f9f6e5b
Tobias Haar <tobias.haar@googlemail.com>
2013-04-09 22:08:43 +0100

    adds transition map to board class

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,9 +1,39 @@
 $(function() {
+  var TransitionMap = {
+    todo: {
+      wip: "stop_working",
+      invalid: "reopen",
+      done: "reopen"
+    },
+    progress: {
+      open: "start_working",
+      reopened: "start_working"
+    },
+    done: {
+      open: "task_is_done",
+      reopened: "task_is_done",
+      wip: "task_is_done"
+    }
+  }
+
+  var TransitionHelper = {
+    getTransition: function(previousState, targetState) {
+      return TransitionMap[targetState][previousState];
+    }
+  }
+
   var Board = {
     initialize: function() {
       $( ".sortable" ).sortable({
         connectWith: ".sortable",
-        placeholder: "board-placeholder-element"
+        placeholder: "board-placeholder-element",
+        receive: function(event, ui) {
+          var previousState = $(ui.item).data("state");
+          var targetState = $(event.target).data("state");
+
+          var transition = TransitionHelper.getTransition(previousState, targetState);
+          alert(transition);
+        }
       }).disableSelection();
     }
   }
