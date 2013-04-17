6b35ac91fbc8e7befd43f92753a488d195df78b1
Tobias Haar <tobias.haar@googlemail.com>
2013-04-17 23:01:09 +0100

    adds abilit to delete labels

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -3,8 +3,7 @@ class LabelsController < ApplicationController
   self.responder = ::ApplicationResponder
   respond_to :html, :json
 
-  before_filter :load_user
-  before_filter :load_project
+  before_filter :load_label_project
   before_filter :require_login
   before_filter :ensure_can_collaborate
 
@@ -16,5 +15,20 @@ class LabelsController < ApplicationController
     respond_with(@label) do |format|
       format.pjax { render @label, :locals => { :issue => nil } }
     end
+  end
+
+  def destroy
+    @label = @project.labels.find_by_id(params[:id])
+
+    @label.destroy
+    respond_with(@label) do |format|
+      format.js { render {} }
+    end
+  end
+
+  private
+  def load_label_project
+    @project = Project.find_by_id(params[:project_id])
+    @user = @project.user
   end
 end
