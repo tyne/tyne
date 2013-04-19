72954f36275636a94b327da621734bbc765844ea
Tobias Haar <tobias.haar@googlemail.com>
2013-04-19 23:32:52 +0100

    adjusts filtering/sorting specs for issues controller

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -98,15 +98,15 @@ describe IssuesController do
         user.reported_issues.create!(:summary => "FOO", :description => "Foo", :issue_type_id => 1) do |issue|
           issue.project_id = project.id
         end
-        assigns(:issues).should == project.issues.order("created_at DESC").limit(25).offset(0)
+        assigns(:issues).should == project.issues.order("issues.created_at DESC").limit(25).offset(0)
 
         # Fallback for field
         get :index, :user => user.username, :key => project.key, :sorting => { :field => 'foo', :order => 'asc' }
-        assigns(:issues).should == project.issues.order("id ASC").limit(25).offset(0)
+        assigns(:issues).should == project.issues.order("issues.id ASC").limit(25).offset(0)
 
         # Fallback for order
         get :index, :user => user.username, :key => project.key, :sorting => { :field => 'created_at', :order => 'foo' }
-        assigns(:issues).should == project.issues.order("created_at ASC").limit(25).offset(0)
+        assigns(:issues).should == project.issues.order("issues.created_at ASC").limit(25).offset(0)
 
         # Custom sorting
         get :index, :user => user.username, :key => project.key, :sorting => { :field => 'issue_type', :order => 'ASC' }
